90db1955aee0cfb85d9a6aec0bb03478287a6eea
Thrail Eank <cashandito@yandex.ru>
2023-05-12 01:29:10 +0300

    Added geos as submodule and spm package

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "geos",
+    platforms: [.iOS(.v9), .macOS("10.9"), .tvOS(.v9), .watchOS(.v2)],
+    products: [
+        .library(
+            name: "geos",
+            type: .dynamic,
+            targets: ["geos"]),
+    ],
+    targets: [
+        .target(
+            name: "geos",
+            publicHeadersPath: "include",
+            cxxSettings: [
+                .define("USE_UNSTABLE_GEOS_CPP_API"),
+                .define("NDEBUG"),
+                .headerSearchPath("include"),
+                .headerSearchPath("src/deps")])
+    ],
+    cxxLanguageStandard: .cxx11
+)
